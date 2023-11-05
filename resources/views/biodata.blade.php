b79ceb1269175c81d4dd7407fcd0c1cd01253bbd
rahulil23 <rahul.ratu662@gmail.com>
2023-11-05 20:58:12 +0700

    [FIX] biodata

---
+++ resources/views/biodata.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Biodata Diri</title>
+
+</head>
+<body>
+    <div class="container">
+        <h1>Hello, World!</h1>
+        <h2>Biodata Diri</h2>
+        <div class="biodata">
+            <p><strong>Nama:</strong> Muhammad rahulil</p>
+            <p><strong>Umur:</strong> 21 tahun</p>
+            <p><strong>Alamat:</strong> Jl. Ir sutami. , Probolinggo</p>
+            <p><strong>Email:</strong> rahul.jeyaker@email.com</p>
+            <p><strong>Telepon:</strong> 085334550580</p>
+            <p><strong>Pendidikan Terakhir:</strong> Manajemen Informatika</p>
+            <p><strong>Pengalaman Kerja:</strong> 3 tahun di PT arkatama</p>
+        </div>
+    </div>
+</body>
+</html>
